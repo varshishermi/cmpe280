633582ac8f3116b8b2fa4370315d0c6198778796
Varshi Gadhamsety <varshigs@gmail.com>
2023-11-28 22:25:14 -0800

    153:

--- client/src/services/userData.js
+++ client/src/services/userData.js
@@ -1,7 +1,7 @@
 // import dotenv  from 'dotenv';
 // dotenv.config();
 //const baseUrl = 'http://localhost:4000';
-const baseUrl = 'https://52.53.242.153:4000';
+const baseUrl = 'http://52.53.242.153:4000';
 // const baseUrl = process.env.SERVER_URL
 
 export async function registerUser(userData) {
